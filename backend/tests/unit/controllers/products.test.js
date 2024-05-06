d2febd76fee697e3a61809d25e14ae05bebd30fd
eduardo maciel <eduardomacielfidelis@gmail.com>
2024-05-06 15:09:28 -0300

    adiciona test a products.test

--- backend/tests/unit/controllers/products.test.js
+++ backend/tests/unit/controllers/products.test.js
@@ -4,6 +4,7 @@ const chaiHttp = require('chai-http');
 const app = require('../../../src/app');
 const connection = require('../../../src/connection');
 const { products } = require('../mocks/product.mocks');
+const productServices = require('../../../src/services/products.services')
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -17,5 +18,12 @@ describe('testa a camada controller', function () {
     const response = await chai.request(app).get('/products');
 
     expect(response).to.have.status(200);
+  });
+  it('ID invalido', async function () {
+    productServices.findSaleById = async () => null;
+    const res = await chai.request(app)
+      .get('/product/201');
+
+    expect(res).to.have.status(404);
   });
 });
