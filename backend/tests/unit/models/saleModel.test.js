600e52d9f8e6912852b1d882f33141a058fd85da
eduardo maciel <eduardomacielfidelis@gmail.com>
2024-05-06 13:27:36 -0300

    cria mocks para sale

--- backend/tests/unit/models/saleModel.test.js
+++ backend/tests/unit/models/saleModel.test.js
@@ -1,8 +1,9 @@
 const sinon = require('sinon');
 const chai = require('chai');
+
 const { expect } = chai;
 const salesModel = require('../../../src/models/sales.models');
-//const mocksSales = require('../mocks/sales.mocks');
+const mocksSales = require('../mocks/sale.mocks');
 const connection = require('../../../src/connection');
 
 describe('testa a model', function () {
@@ -11,5 +12,18 @@ describe('testa a model', function () {
     const result = await salesModel.findAllSales();
 
     expect(result).to.be.an('array');
+  });
+
+  it('testa se retorna um objeto com id passado', async function () {
+    sinon.stub(connection, 'execute').resolves(mocksSales.sales);
+    const result = await salesModel.findSaleByID(1);
+
+    expect(result).to.be.an('object');
+    expect(result.productId).to.equal(1);
+    expect(result.quantity).to.equal(2);
+    expect(result.date).to.equal('2021-09-09T04:54:29.000Z');
+  });
+  afterEach(function () {
+    sinon.restore();
   });
 });
