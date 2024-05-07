fa8acb8314b3007f8b380f2e16f6a09df6f333b9
eduardo maciel <eduardomacielfidelis@gmail.com>
2024-05-07 01:19:44 -0300

    corrige lint

--- backend/tests/unit/productsRouter.test.js
+++ backend/tests/unit/productsRouter.test.js
@@ -1,25 +1,25 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const chaiHttp = require('chai-http');
-const connection = require('../../src/connection')
+const connection = require('../../src/connection');
 const app = require('../../src/app');
-const { products } = require('./mocks/product.mocks')
+const { products } = require('./mocks/product.mocks');
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('testa rota proucts', function () {
-    beforeEach(function () {
-        sinon.stub(connection, 'execute').resolves([products]);
-    })
-    afterEach(function () {
-        connection.execute.restore();
-    })
-        it('deve retornar  products', async function () {
-            const response = await chai.request(app).get('/products');
+  beforeEach(function () {
+    sinon.stub(connection, 'execute').resolves([products]);
+  });
+  afterEach(function () {
+    connection.execute.restore();
+  });
+  it('deve retornar  products', async function () {
+    const response = await chai.request(app).get('/products');
 
-            expect(response).to.have.status(200);
-            expect(response.body).to.be.an('array');
-            expect(response.body).to.be.deep.equal(products)
-        })
-})
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.an('array');
+    expect(response.body).to.be.deep.equal(products);
+  });
+});
